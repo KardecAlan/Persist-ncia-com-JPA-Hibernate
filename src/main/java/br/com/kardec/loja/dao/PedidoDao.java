7845e5add35d1ce3099bd64efa1fa279cfe54a53
Alan Kardec <alankardec05@gmail.com>
2023-11-21 15:51:15 -0300

    Criando relatorio de vendas

--- src/main/java/br/com/kardec/loja/dao/PedidoDao.java
+++ src/main/java/br/com/kardec/loja/dao/PedidoDao.java
@@ -1,6 +1,7 @@
 package br.com.kardec.loja.dao;
 
 import br.com.kardec.loja.modelo.Pedido;
+import br.com.kardec.loja.vo.RelatorioDeVendasVo;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
@@ -21,6 +22,19 @@ public class PedidoDao {
     public BigDecimal valorTotalVendido(){
         String jpql = "SELECT SUM(p.valorTotal) FROM Pedido p";
         return em.createQuery(jpql, BigDecimal.class).getSingleResult();
+    }
+
+    public List<RelatorioDeVendasVo> relatorioDeVendas(){
+        String jpql = "SELECT new br.com.kardec.loja.vo.RelatorioDeVendasVo(" +
+                "produto.nome," +
+                "SUM(item.quantidade)," +
+                "MAX(pedido.data))" +
+                "FROM Pedido pedido " +
+                "JOIN pedido.itens item " +
+                "JOIN item.produto produto " +
+                "GROUP BY produto.nome " +
+                "ORDER BY item.quantidade DESC";
+        return em.createQuery(jpql, RelatorioDeVendasVo.class).getResultList();
     }
 
 }
--- src/main/java/br/com/kardec/loja/testes/CadastroDePedido.java
+++ src/main/java/br/com/kardec/loja/testes/CadastroDePedido.java
@@ -6,9 +6,12 @@ import br.com.kardec.loja.dao.PedidoDao;
 import br.com.kardec.loja.dao.ProdudoDao;
 import br.com.kardec.loja.modelo.*;
 import br.com.kardec.loja.util.JPAUtil;
+import br.com.kardec.loja.vo.RelatorioDeVendasVo;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
 
 public class CadastroDePedido {
 
@@ -19,29 +22,43 @@ public class CadastroDePedido {
         ClienteDao clienteDao = new ClienteDao(em);
 
         Produto produto = produdoDao.buscarPorId(1l);
+        Produto produto2 = produdoDao.buscarPorId(2l);
+        Produto produto3 = produdoDao.buscarPorId(3l);
         Cliente cliente = clienteDao.buscarPorId(1l);
 
         em.getTransaction().begin();
 
 
         Pedido pedido = new Pedido(cliente);
-        pedido.adicionarItem(new ItemPedido(10,pedido, produto));
+        pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+        pedido.adicionarItem(new ItemPedido(40, pedido, produto2));
+
+        Pedido pedido2 = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(2, pedido, produto3));
 
         PedidoDao pedidoDao = new PedidoDao(em);
         pedidoDao.cadastrar(pedido);
+        pedidoDao.cadastrar(pedido2);
 
         em.getTransaction().commit();
 
 
         BigDecimal totalVendido = pedidoDao.valorTotalVendido();
         System.out.println("VALOR TOTAL: " + totalVendido);
-        em.close();
+
+        List<RelatorioDeVendasVo> relatorio = pedidoDao.relatorioDeVendas();
+        relatorio.forEach(System.out::println);
 
     }
 
     private static void popularBancoDeDados() {
         Categoria celulares = new Categoria("CELULARES");
+        Categoria videogames = new Categoria("VIDEOGAMES");
+        Categoria informatica = new Categoria("INFORMATICA");
+
         Produto celular = new Produto("Xiami Redmi", "Barato e bom", new BigDecimal("800"), celulares);
+        Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("3500"), videogames);
+        Produto macbook = new Produto("Macbook", "Macbook para retirada", new BigDecimal("16000"), informatica);
 
         Cliente cliente = new Cliente("Kardec", "987654");
 
@@ -53,7 +70,12 @@ public class CadastroDePedido {
         em.getTransaction().begin();
 
         categoriaDao.cadastrar(celulares);
+        categoriaDao.cadastrar(videogames);
+        categoriaDao.cadastrar(informatica);
+
         produtoDao.cadastrar(celular);
+        produtoDao.cadastrar(videogame);
+        produtoDao.cadastrar(macbook);
         clienteDao.cadastrar(cliente);
 
         em.getTransaction().commit();
