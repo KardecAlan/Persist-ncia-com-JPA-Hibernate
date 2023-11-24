7d45200d159e419686a260f02a57b68e328d1c46
Alan Kardec <alankardec05@gmail.com>
2023-11-24 01:13:47 -0300

    Usando o Join Fetch para criar consultas planejadas

--- src/main/java/br/com/kardec/loja/testes/PerformanceConsultas.java
+++ src/main/java/br/com/kardec/loja/testes/PerformanceConsultas.java
@@ -14,8 +14,10 @@ public class PerformanceConsultas {
     public static void main(String[] args){
         popularBancoDeDados();
         EntityManager em = JPAUtil.getEntityManager();
-        Pedido pedido = em.find(Pedido.class, 1l);
-        System.out.println(pedido.getItens().size());
+        PedidoDao pedidoDao = new PedidoDao(em);
+        Pedido pedido = pedidoDao.buscarPedidoCOmCLiente(1l);
+        em.close();
+        System.out.println(pedido.getCliente().getNome());
 
     }
 
